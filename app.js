97c7b24083fe67bb90c41b0199b16a725bd34045
disco-coder <1koblizek1@gmail.com>
2019-11-12 13:18:57 +0100

    Make basic functions of calculator work

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
+// Display content
 let displayValue = document.querySelector("#display-text");
 
-let buttons = document.querySelectorAll(".button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => displayValue.textContent = `${displayValue.textContent + button.textContent}`)
+// Store operator
+let currentOperator = "";
+let operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach((operator) => {
+    operator.addEventListener("click", () => currentOperator = `${operator.id}`)
 })
+
+// Plus-minus button
+let plusMinusButton = document.querySelector(".plus-minus");
+plusMinusButton.addEventListener("click", () => displayValue.textContent = displayValue.textContent);
+
+// Clickable number buttons
+let numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if (currentOperator != "") {
+            displayValue.textContent = operate(currentOperator, parseInt(displayValue.textContent), parseInt (number.textContent));
+            currentOperator = "";
+        } else {
+            displayValue.textContent = `${displayValue.textContent + number.textContent}`;
+        }
+    });
+});
+
+// Clear display
+let clearButton = document.querySelector(".clear")
+clearButton.addEventListener("click", () => displayValue.textContent = "");
+
+// Percent function
+let percentButton = document.querySelector(".percent");
+percentButton.addEventListener("click", () => displayValue.textContent = `${displayValue.textContent * 0.01}`)
+
 
 
 // Basic math operators
