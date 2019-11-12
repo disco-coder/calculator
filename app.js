5606d1d7ed49c2b5c3364351c7dbc97272ad30b7
disco-coder <1koblizek1@gmail.com>
2019-11-12 09:19:46 +0100

    Make buttons clickable

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+let displayValue = document.querySelector("#display-text");
+
+let buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => displayValue.textContent = `${displayValue.textContent + button.textContent}`)
+})
+
 
 // Basic math operators
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
-
 
 function operate(operator, a, b) {
     if (operator === "add") return add(a, b);
