8f6ec48a739895dc37da46119c7d3bc9ad7f281a
disco-coder <1koblizek1@gmail.com>
2019-11-13 19:43:09 +0100

    Refactor functions

--- app.js
+++ app.js
@@ -2,83 +2,35 @@
 let displayValue = document.querySelector("#display-text");
 let currentValue = "";
 let currentOperator = "";
-
-// Stores bool whether equals was pressed, helper for clearing display
 let pressedEquals;
 
-let operatorButtons = document.querySelectorAll(".operator");
-operatorButtons.forEach((operator) => {
-    operator.addEventListener("click", () => {
-        
-        // Operator onclick inset shadow
-        operatorButtons.forEach((operator) => operator.style.boxShadow = "");
-        operator.style.boxShadow = "inset 0 0 0 2px #000";
-
-        if (currentOperator != "" && currentValue != "") {
-            displayValue.textContent = operate(currentOperator, parseFloat(currentValue), parseFloat(displayValue.textContent));
-            currentValue = "";
-        }
-        
-        if (displayValue.textContent) currentOperator = `${operator.id}`;
-    });
-})
+// Operator buttons
+const operatorButtons = document.querySelectorAll(".operator");
+selectOperator();
 
 // Decimal button
-let decimalButton = document.querySelector(".point");
-decimalButton.addEventListener("click", () => {
-    if (displayValue.textContent.slice(-1) != ".") {
-        displayValue.textContent = displayValue.textContent + ".";
-    }
-})
+const decimalButton = document.querySelector(".point");
+makeDecimal();
 
 // Plus-minus button
-let plusMinusButton = document.querySelector(".plus-minus");
+const plusMinusButton = document.querySelector(".plus-minus");
 plusMinusButton.addEventListener("click", () => displayValue.textContent = -displayValue.textContent);
 
 // Clickable number buttons
-let numbers = document.querySelectorAll(".number");
-numbers.forEach((number) => {
-    number.addEventListener("click", () => {
-        if (displayValue.textContent == "" && number.textContent == 0) {
-            return;
-        }
-
-        if (currentOperator != "" && currentValue == "") {
-            currentValue = displayValue.textContent;
-            displayValue.textContent = "";
-        }
-        if (pressedEquals) {
-            displayValue.textContent = "";
-            pressedEquals = false;
-        }
-        displayValue.textContent = `${displayValue.textContent + number.textContent}`;
-    });
-});
+const numbers = document.querySelectorAll(".number");
+displayNumber();
 
 // Equals button
-let equalsButton = document.querySelector(".equals");
-equalsButton.addEventListener("click", () => {
-    if (currentValue != "") {
-        displayValue.textContent = operate(currentOperator, parseFloat(currentValue), parseFloat(displayValue.textContent));
-        currentValue = "";
-        currentOperator = "";
-        pressedEquals = true;
-    }
-    operatorButtons.forEach((operator) => operator.style.boxShadow = "");
-})
+const equalsButton = document.querySelector(".equals");
+getResult();
 
 // Clear display
-let clearButton = document.querySelector(".clear")
-clearButton.addEventListener("click", () => {
-    operatorButtons.forEach((operator) => operator.style.boxShadow = "");
-    displayValue.textContent = "";
-    currentOperator = "";
-    currentValue = "";
-});
+const clearButton = document.querySelector(".clear")
+clearDisplay();
 
 // Percent function
-let percentButton = document.querySelector(".percent");
-percentButton.addEventListener("click", () => displayValue.textContent = `${parseInt(displayValue.textContent) * 0.01}`);
+const percentButton = document.querySelector(".percent");
+getPercentage();
 
 // Basic math operators
 let add = (a, b) => a + b;
@@ -91,4 +43,79 @@ function operate(operator, a, b) {
     if (operator === "subtract") return Math.round(subtract(a, b) * 1000) / 1000;
     if (operator === "multiply") return Math.round(multiply(a, b) * 1000) / 1000;
     if (operator === "divide") return Math.round(divide(a, b) * 1000) / 1000;
+}
+
+function removeBoxShadow() {
+    operatorButtons.forEach((operator) => operator.style.boxShadow = "");
+}
+
+function getPercentage() {
+    percentButton.addEventListener("click", () => displayValue.textContent = `${parseInt(displayValue.textContent) * 0.01}`);
+}
+
+function clearDisplay() {
+    clearButton.addEventListener("click", () => {
+        removeBoxShadow();
+        displayValue.textContent = "";
+        currentOperator = "";
+        currentValue = "";
+    });
+}
+
+function getResult() {
+    equalsButton.addEventListener("click", () => {
+        if (currentValue != "") {
+            displayValue.textContent = operate(currentOperator, parseFloat(currentValue), parseFloat(displayValue.textContent));
+            currentValue = "";
+            currentOperator = "";
+            pressedEquals = true;
+        }
+        removeBoxShadow();
+    })
+}
+
+function displayNumber() {
+    numbers.forEach((number) => {
+        number.addEventListener("click", () => {
+            if (displayValue.textContent == "" && number.textContent == 0) {
+                return;
+            }
+    
+            if (currentOperator != "" && currentValue == "") {
+                currentValue = displayValue.textContent;
+                displayValue.textContent = "";
+            }
+            if (pressedEquals) {
+                displayValue.textContent = "";
+                pressedEquals = false;
+            }
+            displayValue.textContent = `${displayValue.textContent + number.textContent}`;
+        });
+    });
+}
+
+function selectOperator() {
+    operatorButtons.forEach((operator) => {
+        operator.addEventListener("click", () => {
+            
+            // Operator onclick inset shadow
+            removeBoxShadow();
+            operator.style.boxShadow = "inset 0 0 0 2px #000";
+    
+            if (currentOperator != "" && currentValue != "") {
+                displayValue.textContent = operate(currentOperator, parseFloat(currentValue), parseFloat(displayValue.textContent));
+                currentValue = "";
+            }
+            
+            if (displayValue.textContent) currentOperator = `${operator.id}`;
+        });
+    })
+}
+
+function makeDecimal() {
+    decimalButton.addEventListener("click", () => {
+        if (displayValue.textContent.slice(-1) != ".") {
+            displayValue.textContent = displayValue.textContent + ".";
+        }
+    })
 }
